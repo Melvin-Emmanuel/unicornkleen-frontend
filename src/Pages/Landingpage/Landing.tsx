0c7ed163aaeecd930a5e13f3d6315f1ec4873dac
Melvin-Emmanuel <emmanulmelv@gmail.com>
2023-11-12 05:29:40 +0100

    added header and footer components

--- src/Pages/Landingpage/Landing.tsx
+++ src/Pages/Landingpage/Landing.tsx
@@ -18,6 +18,10 @@ import Works from "../../Components/Commons/Cards/Works";
 // import Client from "../../components/Cards/Client";
 import Client from "../../Components/Commons/Cards/Client";
 import CountDown from "../../Components/Commons/Cards/CountDown";
+import Timer from "../../Components/Blocks/Timer";
+import Header from "../../Components/Blocks/Header";
+import Services from "../../Components/Commons/Cards/Services";
+import Footer from "../../Components/Blocks/Footer";
 // import Card2 from "../../Components/Commons/Cards/Card2";
 // import Blog from '../../components/Cards/Blog'
 
@@ -25,6 +29,8 @@ const Landing = () => {
   return (
     <div className="bg-[rgb(18,57,172)] w-[100%] min-h-[90vh]">
       {/* <Header /> */}
+      <Timer />
+      <Header/>
       <Card1 />
       <Card2 />
       <Card3 />
@@ -32,6 +38,8 @@ const Landing = () => {
       <CountDown />
       <PaymentCard />
       <Client />
+      <Services />
+      <Footer/>
       {/* <Services /> */}
 
       {/* <Blog /> */}
