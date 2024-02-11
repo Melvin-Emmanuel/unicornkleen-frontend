ac317e9b7bc0838840e84a042753355bcd8e7119
Melvin-Emmanuel <emmanulmelv@gmail.com>
2024-02-11 19:59:07 +0100

    last commit before production

--- src/utils/AxiosConfig.ts
+++ src/utils/AxiosConfig.ts
@@ -8,9 +8,11 @@
 // export default Instance
 
 import axios from "axios"
+const baseUrl = "http://localhost:5200/api/v1"
+const baseURL2 = "https://unicornklee.onrender.com/api/v1";
 
 const Instance = axios.create({
-  baseURL: "http://localhost:5200/api/v1",
+  baseURL: baseURL2,
 });
 
 export default Instance;
